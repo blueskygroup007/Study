8fddae4b967413ecdede6bd1b977815945365568
4000m <16733682@qq.com>
2021-12-12 16:15:44 +0800

    4.在fragment中实现sence的item点击事件处理 待完成

--- app/src/main/java/com/bluesky/study/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/bluesky/study/ui/gallery/GalleryFragment.java
@@ -1,5 +1,9 @@
 package com.bluesky.study.ui.gallery;
 
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -23,9 +27,11 @@ import java.util.List;
 
 public class GalleryFragment extends Fragment {
 
+    private static final int REQUEST_CODE_GET_CONTENT = 1;
     private GalleryViewModel galleryViewModel;
     private FragmentGalleryBinding binding;
     private GalleryRecyclerViewAdapter mAdapter;
+    private List<Sence> mListSence = new ArrayList<>();
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -50,17 +56,52 @@ public class GalleryFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         binding.rvSences.setLayoutManager(new GridLayoutManager(requireContext(), 5));
         mAdapter = new GalleryRecyclerViewAdapter(binding.rvSences);
-        List<Sence> list = new ArrayList<>();
-        mAdapter.setData(list);
+        mListSence = getSences();
+        mAdapter.setData(mListSence);
         mAdapter.setOnItemClickListener(new OnItemClickListener() {
             @Override
             public void onItemClick(View view, int Position) {
                 //TODO 从外面实现RecyclerView的点击事件的一种方式,放在了adapter中.
                 Log.e("errorlog", "Position=  " + Position);
+                if (Position == mListSence.size()) {
+                    //如果是最后一个item.即+号
+                    Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+                    //文件类型
+                    intent.setType("video/*;image/*");
+                    //支持多选(长按)
+                    intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                    //表示intent仅希望查询能使用ContentResolver.openFileDescriptor(Uri,String)打开的Uri
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    //startActivityForResult(intent, REQUEST_CODE_GET_CONTENT);
+                    //包装intent的用法
+                    startActivityForResult(Intent.createChooser(intent, "选择文件"), REQUEST_CODE_GET_CONTENT);
+
+                } else {
+                    //如果是某条视频
+                    //TODO 播放
+
+                }
             }
         });
         binding.rvSences.setAdapter(mAdapter);
 
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK && requestCode == REQUEST_CODE_GET_CONTENT) {
+            Uri uri = data.getData();
+            Log.e("errorlog", "文件路径:" + uri.getPath().toString());
+            //TODO 将文件路径保存到preference中.并更新senceList
+
+        }
+    }
+
+    @NonNull
+    private List<Sence> getSences() {
+        List<Sence> list = new ArrayList<>();
+        return list;
     }
 
     @Override
